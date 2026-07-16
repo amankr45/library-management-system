1c46b57164c6a6eb5c9ab1c07bbf547cfe9197dd
amankr45 <amankrpes@gmail.com>
2026-07-16 20:03:41 +0530

    Unrelated packages and imports are removed

--- src/main/java/com/aman/LibraryManagementSystem/service/BookService.java
+++ src/main/java/com/aman/LibraryManagementSystem/service/BookService.java
@@ -4,7 +4,6 @@ import com.aman.LibraryManagementSystem.dto.request.BookRequest;
 import com.aman.LibraryManagementSystem.dto.response.BookResponse;
 import org.springframework.data.domain.Page;
 
-import java.util.List;
 
 public interface BookService {
 
